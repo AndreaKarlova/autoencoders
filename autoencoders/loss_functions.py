1249d7a0d7d249f353e6aace504e4ea93c881d64
Andrea <andrea.karlova@gmail.com>
2020-10-09 16:36:58 +0100

    added: beta-vae, character processor

--- autoencoders/loss_functions.py
+++ autoencoders/loss_functions.py
@@ -3,13 +3,13 @@ from  tensorflow_probability import distributions as tfd
 
 
 @tf.function
-def loss_elbo(model, x, **call_args):
+def loss_elbo(model, x, kl_weight=1., **call_args):
     model_outputs = model(x, **call_args)
     kl_regulariser = tf.reduce_mean(tf.reduce_sum(model.kl_loss(model_outputs), axis=1))
     log_probabilities = model.decoder_dist(
         loc=model_outputs['decoded'], eps_scale=model.decoder_eps_scale).log_prob(x)
     likelihood = tf.reduce_mean(tf.reduce_sum(log_probabilities, axis=[1, 2, 3]))
-    negative_elbo = -likelihood + kl_regulariser 
+    negative_elbo = -likelihood + kl_weight * kl_regulariser 
     return dict(final=negative_elbo, likelihood=likelihood, kl=kl_regulariser)
 
 @tf.function
